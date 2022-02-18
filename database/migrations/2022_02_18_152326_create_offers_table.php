785c3d084f8d89aa1faa35d8649a4f2c62b8cde2
AbdelrhmanSaied98 <abdelrahmansaied992@gmail.com>
2022-02-18 18:32:05 +0200

    Create migrations

---
+++ database/migrations/2022_02_18_152326_create_offers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('doc_id');
+            $table->string('name');
+            $table->float('discount');
+            $table->foreign('doc_id')->references('id')->on('doctors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
---
+++ database/migrations/2022_02_18_161308_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->unsignedBigInteger('appointment_id');
+            $table->time('patient_time');
+            $table->unsignedBigInteger('patient_id');
+            $table->unsignedBigInteger('doc_id');
+            $table->foreign('appointment_id')->references('id')->on('appointments');
+            $table->foreign('patient_id')->references('id')->on('patients');
+            $table->foreign('doc_id')->references('id')->on('doctors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
